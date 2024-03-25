40fab0a23b93d7007ba07c2c1671629cefa1c02d
stynun <stynun@gmail.com>
2024-03-25 23:16:47 +0900

    string을 활용한 간단한 big_integer 구현 코드 추가

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+void enterNumber();
+void sumNumber();
+void subNumber();
+void printResult();
+
+int main() {
+    enterNumber();
+
+    sumNumber();
+    subNumber();
+
+    printResult();
+
+    return 0;
+}
---
+++ util.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+std::string n1, n2, tmpStr, sumStr, subStr;
+int lensub;
+
+void enterNumber() {
+    std::cout << "First number  >> ";
+    getline(std::cin, n1);
+
+    std::cout << "Second number >> ";
+    getline(std::cin, n2);
+
+    lensub = n1.length() - n2.length();
+    
+    if (lensub == 0) {
+        n1.insert(0, "0");
+        n2.insert(0, "0");
+        for (int i = 0; i < n2.length(); i++) {
+            sumStr.insert(0, "0");
+            subStr.insert(0, "0");
+
+        }
+    }  
+    else {
+        if (lensub < 0) {
+            tmpStr.append(n1);
+            n1.clear();
+            n1.append(n2);
+            n2.clear();
+            n2.append(tmpStr);
+        }
+
+        int times = n1.length() - n2.length();
+        for (int i = 0; i <= times; i++) {
+            n2.insert(0, "0");
+        }
+        n1.insert(0, "0");
+        for (int i = 0; i < n1.length(); i++) {
+            sumStr.insert(0, "0");
+            subStr.insert(0, "0");
+
+        }
+    }
+}
+
+void sumNumber() {
+    int tmp1, tmp2, tmp3, tmp4, tmp5, sum = 1;
+    for (int i = sumStr.length() - 1; i >= 1; i--) {
+        tmp1 = n1[i] -'0';
+        tmp2 = n2[i] -'0';
+        tmp3 = sumStr[i] - '0';
+        sum = tmp1 + tmp2;
+        tmp4 = (tmp3 + sum) / 10;
+        tmp5 = (tmp3 + sum) % 10;
+        
+        sumStr[i - 1] = tmp4 + '0';
+        sumStr[i] = tmp5 + '0';
+    }
+
+    if (sumStr[0] == '0')
+        sumStr.erase(0, 1);
+}
+
+void subNumber() {
+    int tmp1, tmp2, tmp3, sub;
+    for (int i = subStr.length() - 1; i >= 0; i--) {
+        tmp1 = n1[i] - '0';
+        tmp2 = n2[i] - '0';
+        tmp3 = n1[i - 1] - '0';
+        sub = tmp1 - tmp2;
+        if (sub < 0) {
+            if (i < 2) {
+                subStr.replace(0, 1, "-");
+                subStr[i] = sub - sub * 2 + '0';
+                break;
+            }
+            else {
+                n1[i - 1] = (tmp3 - 1) + '0';
+                sub += 10;
+            }
+        }
+
+        subStr[i] = sub + '0';
+    }
+
+    int j = subStr.length() - 1;
+    while (j != 0) {
+        if (subStr[0] == '0') 
+            subStr.erase(0, 1);
+        j--;
+    }
+
+    if (lensub < 0)
+        subStr.insert(0, "-");
+}
+
+void printResult() {
+    std::cout << "Sum           >> ";
+    for (int i = 0; i < sumStr.length(); i++) {
+        std::cout << sumStr[i];
+    }  
+    std::cout << std::endl;
+
+    std::cout << "Sub           >> ";
+    for (int i = 0; i < subStr.length(); i++) {
+        std::cout << subStr[i];
+    }  
+    std::cout << std::endl;
+}
