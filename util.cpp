016de8b00d7708e4acd961085c291bac3d666aa2
stynun <89558804+stynun@users.noreply.github.com>
2024-03-27 05:54:59 +0900

    fix: 두 수가 같은 자리 수인데 두번째 숫자가 더 크면 빼기에서 '-'가 안나오는 현상 수정

--- util.cpp
+++ util.cpp
@@ -4,6 +4,7 @@
 
 std::string n1, n2, tmpStr, sumStr, subStr;
 int lensub;
+bool change = false;
 
 void enterNumber() {
     std::cout << "First number  >> ";
@@ -15,6 +16,18 @@ void enterNumber() {
     lensub = n1.length() - n2.length();
     
     if (lensub == 0) {
+        for (int i = 0; i < n1.length(); i++) {
+            if (n1[i] -'0' < n2[i] -'0') {
+                tmpStr.append(n1);
+                n1.clear();
+                n1.append(n2);
+                n2.clear();
+                n2.append(tmpStr);
+                change = true;
+                break;
+            }
+        }
+
         n1.insert(0, "0");
         n2.insert(0, "0");
         for (int i = 0; i < n2.length(); i++) {
@@ -71,15 +84,8 @@ void subNumber() {
         tmp3 = n1[i - 1] - '0';
         sub = tmp1 - tmp2;
         if (sub < 0) {
-            if (i < 2) {
-                subStr.replace(0, 1, "-");
-                subStr[i] = sub - sub * 2 + '0';
-                break;
-            }
-            else {
-                n1[i - 1] = (tmp3 - 1) + '0';
-                sub += 10;
-            }
+            n1[i - 1] = (tmp3 - 1) + '0';
+            sub += 10;
         }
 
         subStr[i] = sub + '0';
@@ -92,7 +98,7 @@ void subNumber() {
         j--;
     }
 
-    if (lensub < 0)
+    if (lensub < 0 || change)
         subStr.insert(0, "-");
 }
 
